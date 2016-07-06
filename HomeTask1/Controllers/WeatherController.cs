5e357a168302028a2381009505cb0f515e271eed
Olena Rehush <olena_rehush@ukr.net>
2016-07-07 01:40:47 +0300

    First version of project was created.

---
+++ HomeTask1/Controllers/WeatherController.cs
@@ -0,0 +1,44 @@
+﻿using HomeTask1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeTask1.Controllers
+{
+    public class WeatherController : Controller
+    {
+        private WeatherService ws;
+       
+
+        //
+        // GET: /Weather/
+        public ActionResult Index(string city="Lviv")
+        {
+            ws = new WeatherService("http://api.openweathermap.org/data/2.5/weather?q={0}&appid=fce02a07226c189ccac0cdbcb3d4325a");
+            var x=ws.GetWeather<WeatherObject>(city);
+            ViewBag.City = x.name;
+            ViewBag.Weather = x;
+            return View();
+        }
+
+        public ActionResult ThreeDays(string city = "Lviv")
+        {
+            ws = new WeatherService("http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&units=metric&APPID=fce02a07226c189ccac0cdbcb3d4325a");
+            var x = ws.GetWeather<WeatherObjectList>(city);
+            ViewBag.City = x.city.name;
+            ViewBag.Weather = x.list.Take(3);
+            return View();
+        }
+
+        public ActionResult SevenDays(string city = "Lviv")
+        {
+            ws = new WeatherService("http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&units=metric&APPID=fce02a07226c189ccac0cdbcb3d4325a");
+            var x = ws.GetWeather<WeatherObjectList>(city);
+            ViewBag.City = x.city.name;
+            ViewBag.Weather = x.list;
+            return View();
+        }
+	}
+}
---
+++ HomeTask1/Models/WeatherService.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace HomeTask1.Models
+{
+    public class WeatherService
+    {
+        private string linkFormat;
+
+        public WeatherService(string linkFormat)
+        {
+            this.linkFormat = linkFormat;
+        }
+
+        public T GetWeather<T>(string city)
+        {
+
+            
+            {
+                // ... Use HttpClient.
+                using (HttpClient client = new HttpClient())
+                using (HttpResponseMessage response = client.GetAsync(String.Format(linkFormat, city)).Result)
+                using (HttpContent content = response.Content)
+                {
+                    // ... Read the string.
+                    string result = content.ReadAsStringAsync().Result;
+
+                    return JsonConvert.DeserializeObject<T>(result);
+                }
+            }
+            
+        }
+    }
+    
+}
